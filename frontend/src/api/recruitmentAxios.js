3c59bc28f36602a9764476abec3f4ab2616359a2
mechaia_feriel <f.mechaia@esi-sba.dz>
2026-05-24 11:54:59 +0100

    gg

--- frontend/src/api/recruitmentAxios.js
+++ frontend/src/api/recruitmentAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const recruitmentApi = axios.create({
-    baseURL: "http://localhost:8083/api/recruitment",
+    baseURL: "http://localhost:90/api",
 });
 
 recruitmentApi.interceptors.request.use((config) => {
