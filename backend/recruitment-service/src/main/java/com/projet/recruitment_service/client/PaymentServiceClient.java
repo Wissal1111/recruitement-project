2f024bef8caf65a18c9788ab5b7f36503c09e234
mechaia_feriel <f.mechaia@esi-sba.dz>
2026-06-01 23:04:52 +0100

    ooo

--- backend/recruitment-service/src/main/java/com/projet/recruitment_service/client/PaymentServiceClient.java
+++ backend/recruitment-service/src/main/java/com/projet/recruitment_service/client/PaymentServiceClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import com.projet.recruitment_service.config.FeignConfig;
 
 import java.util.Map;
 
